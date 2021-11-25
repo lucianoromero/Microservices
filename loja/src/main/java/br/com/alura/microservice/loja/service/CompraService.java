bd25d45cb191491c8b81d89c33e53ff1303b65c9
Romero <40298952+lucianoromero@users.noreply.github.com>
2021-11-25 10:24:11 -0300

    04 Implementacao - Implementacao com Spring Feign
    modificando a implementacao para ultilizar o spring feing

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,28 +1,23 @@
 package br.com.alura.microservice.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import br.com.alura.microservice.loja.client.FornecedorClient;
 import br.com.alura.microservice.loja.dto.CompraDTO;
 import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
 
 @Service
 public class CompraService {
-	
+
 	@Autowired
-	private RestTemplate client;
+	private FornecedorClient fornecedorClint;
 
 	public void realizaCompra(CompraDTO compra) {
-		
-		ResponseEntity<InfoFornecedorDTO> exchange = 
-			client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(),
-			HttpMethod.GET, null, InfoFornecedorDTO.class);
-		
-		System.out.println(exchange.getBody().getEndereco());
-		
+
+		InfoFornecedorDTO info = fornecedorClint.getInfoPorEstado(compra.getEndereco().getEstado());
+		System.out.println(info.getEndereco());
+
 	}
 
 }
