6c4ce1ffebcbcd2673230acd6678c941de0ab3e0
Romero <40298952+lucianoromero@users.noreply.github.com>
2021-11-25 10:51:53 -0300

    04 Atualizando - Atualizando a API Fornecedor
    Atualizando a API Fornecedor, acrecentando novas funcionalidades

--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/repository/InfoRepository.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/repository/InfoRepository.java
@@ -1,4 +1,4 @@
-package br.com.alura.microservice.fornecedor.service;
+package br.com.alura.microservice.fornecedor.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/service/InfoService.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/service/InfoService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.microservice.fornecedor.model.InfoFornecedor;
+import br.com.alura.microservice.fornecedor.repository.InfoRepository;
 
 @Service
 public class InfoService {
