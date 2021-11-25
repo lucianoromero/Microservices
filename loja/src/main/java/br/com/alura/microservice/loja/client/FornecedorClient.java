04f2de2ee7f707a4e7cda5baa39ad9fb5cd04eab
Romero <40298952+lucianoromero@users.noreply.github.com>
2021-11-25 10:25:45 -0300

    04 Implementacao - Implementacao com Spring Feign
    Adicionando o commit da interface que implementa o feing

---
+++ loja/src/main/java/br/com/alura/microservice/loja/client/FornecedorClient.java
@@ -0,0 +1,15 @@
+package br.com.alura.microservice.loja.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
+
+@FeignClient("fornecedor")
+public interface FornecedorClient {
+
+	@RequestMapping("/info/{estado}")
+	InfoFornecedorDTO getInfoPorEstado(@PathVariable String estado);
+
+}
